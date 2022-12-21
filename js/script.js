e667a4ffd2303ff124e72a0790837de807ed5c2e
shebnem reshidova <shabnamrashidova90@gmail.com>
2022-12-21 18:31:48 +0400

    additional new page

--- js/script.js
+++ js/script.js
@@ -148,7 +148,10 @@ $(function(){
     $(function(){
         $(".review-btn").click(()=>{
             $(".success-popup").addClass("show")
-            $(".comemnt-popup").removeClass("show")
+          setTimeout(() => {
+            $(".success-popup").removeClass("show")
+          }, 2000);
+            $(".comment-popup").removeClass("show")
         })
     })
     // add skills
@@ -205,3 +208,41 @@ chooseBtn.forEach((btn)=>{
      
     })
 })
+$(function () {
+    var parent = document.querySelector(".range-slider");
+    if (!parent) return;
+  
+    var rangeS = parent.querySelectorAll("input[type=range]"),
+      numberS = parent.querySelectorAll("input[type=number]");
+  
+    rangeS.forEach(function (el) {
+      el.oninput = function () {
+        var slide1 = parseFloat(rangeS[0].value),
+          slide2 = parseFloat(rangeS[1].value);
+  
+        if (slide1 > slide2) {
+          [slide1, slide2] = [slide2, slide1];
+          
+        }
+  
+        numberS[0].value = slide1;
+        numberS[1].value = slide2;
+      };
+    });
+  
+    numberS.forEach(function (el) {
+      el.oninput = function () {
+        var number1 = parseFloat(numberS[0].value),
+          number2 = parseFloat(numberS[1].value);
+  
+        if (number1 > number2) {
+          var tmp = number1;
+          numberS[0].value = number2;
+          numberS[1].value = tmp;
+        }
+  
+        rangeS[0].value = number1;
+        rangeS[1].value = number2;
+      };
+    });
+  })();
